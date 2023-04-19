376ea2b9739090598fbe32d9a6876a49cbd25753
KHminor <ghdals2020@gmail.com>
2023-04-19 10:20:17 +0900

    Design: 수정 Main

--- front-server/src/Components/Main/Main.tsx
+++ front-server/src/Components/Main/Main.tsx
@@ -6,22 +6,78 @@ import MyHomeAsset from './MyHomeAsset';
 function Main(): JSX.Element {
   return (
     <>
-      <div className="absolute flex items-center justify-center w-full bottom-[16%]">
+      <div className="absolute flex items-center justify-center w-full h-full border-4">
         <img
-          className="object-contain scale-125"
-          src="/images/toys/floor2.png"
+          aria-label="마카롱"
+          className="absolute object-contain scale-110 mt-[21rem]"
+          src="/images/toys/floor.png"
+          alt=""
+        />
+        <img
+          aria-label="은행"
+          className="absolute z-10 object-contain w-[11rem] h-[11rem] top-20 right-1/2"
+          src="/images/toys/bank.png"
+          alt=""
+        />
+        <img
+          aria-label="정보상"
+          className="absolute z-10 object-contain w-[13rem] h-[13rem] top-36 right-[14rem]"
+          src="/images/toys/info.png"
+          alt=""
+        />
+        <img
+          aria-label="방문하기"
+          className="absolute z-10 object-contain w-[13rem] h-[13rem] top-44 left-56"
+          src="/images/toys/visit.png"
+          alt=""
+        />
+        <img
+          aria-label="랭킹"
+          className="absolute z-10 object-contain w-[8rem] h-[8rem] top-80 right-[15rem]"
+          src="/images/toys/rank.png"
+          alt=""
+        />
+        <img
+          aria-label="경매장"
+          className="absolute z-10 object-contain w-[15rem] h-[15rem] top-80 left-52"
+          src="/images/toys/auction.png"
+          alt=""
+        />
+        <img
+          aria-label="뽑기상점"
+          className="absolute z-10 object-contain w-[12rem] h-[12rem] bottom-0 right-1/2"
+          src="/images/toys/gatcha.png"
+          alt=""
+        />
+        <img
+          aria-label="주식거래소"
+          className="absolute z-10 object-contain w-[15rem] h-[15rem] -mb-[25rem] ml-[42rem]"
+          src="/images/toys/chart.png"
+          alt=""
+        />
+        <img
+          aria-label="가이드"
+          className="absolute z-10 object-contain w-[10rem] h-[10rem] -mb-[25rem] left-0 border-4"
+          src="/images/toys/guide.png"
           alt=""
         />
       </div>
       <div className="w-full">
-        {/* <Bathroom /> */}
-        <Canvas>
+        <Canvas
+          orthographic
+          camera={{
+            left: -1,
+            right: 1,
+            top: 1,
+            bottom: -1,
+            near: 0.1,
+            far: 1000,
+            zoom: 100
+          }}>
+          {/* <OrbitControls /> */}
           <ambientLight intensity={0.5} />
           <pointLight distance={2000} position={10} power={8} />
-
-          {/* <Bathroom len={0.02} pos={[-0.2, 0, -15]} rot={[2.15, 0, 0.5]} /> */}
-          <Bathroom len={0.008} pos={[-0.2, -1.5, -1.8]} rot={[1.6, 0, 0]} />
-          {/* <MyHomeAsset len={0.008} pos={[-0.2, -1.5, -1.8]} rot={[1.6, 0, 0]} /> */}
+          <Bathroom len={0.0055} pos={[0, -1.3, -8]} rot={[1.75, 0, 0.2]} />
         </Canvas>
       </div>
     </>
