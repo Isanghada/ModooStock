d58c35d74367250e5b9b53538cf87077444994c7
HanSangHyun <xhaktm3344@gmail.com>
2023-04-20 02:52:22 +0900

    Design : 추가 로그인 화면 크기 반응형

--- front-server/src/Layout.tsx
+++ front-server/src/Layout.tsx
@@ -2,16 +2,31 @@ import { Outlet } from 'react-router-dom';
 import { AnimatePresence, motion } from 'framer-motion';
 import { useAppSelector } from 'Store/hooks';
 import Login from 'Components/Login/Login';
+import { useEffect, useState } from 'react';
 
 function Layout(): JSX.Element {
-  // 로그인 창
+  // 현재 브라우저 윈도우 너비 값
+  const [screenWidth, setScreenWidth] = useState<number>(0);
+
+  // 로그인 창 상태
   const loginStatus = useAppSelector((state) => {
     return state.loginStatus;
   });
+
+  useEffect(() => {
+    // 창크기 변할때마다 실행
+    const updateScreenWidth = () => {
+      const newWidth = window.innerWidth;
+      setScreenWidth(newWidth);
+    };
+    window.addEventListener("resize", updateScreenWidth);
+    return () => window.removeEventListener("resize", updateScreenWidth) 
+  }, [])
+
   return (
     <>
       <div className="bg-[#FEF9F9] w-screen relative flex">
-        <div className="container relative flex w-full h-screen mx-auto lg:max-w-screen-xl">
+        <div className={`${loginStatus ? "w-[60vw] lg:w-[70vw]" : "w-full"} container relative flex h-screen mx-auto lg:max-w-screen-xl`}>
           <Outlet />
         </div>
         {/* 로그인 컴포넌트 */}
@@ -20,7 +35,7 @@ function Layout(): JSX.Element {
             <motion.div
               className="max-h-screen min-h-full"
               initial={{ width: 0 }}
-              animate={{ width: '40vw' }}
+              animate={screenWidth <= 1024 ? { width: '40vw'} : { width: '30vw'}}
               exit={{ width: 0, opacity: 0 }}
               transition={{
                 duration: 0.7,
