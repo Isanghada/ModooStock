93926eefd22ceefcbea72de2e0f06fbc813ca396
KHminor <ghdals2020@gmail.com>
2023-04-19 17:31:49 +0900

    Design: 수정 main

--- front-server/src/Components/Main/Main.tsx
+++ front-server/src/Components/Main/Main.tsx
@@ -1,12 +1,38 @@
 import { OrbitControls, OrthographicCamera } from '@react-three/drei';
-import { Canvas } from '@react-three/fiber';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { useEffect, useState } from 'react';
 import Bathroom from './Bathroom';
 import MyHomeAsset from './MyHomeAsset';
 
 function Main(): JSX.Element {
+  const [floor, setFloor] = useState(
+    window.screen.width <= 1280
+      ? `${2 + (window.screen.width - 1024) * (1 / 140)}rem`
+      : '4rem'
+  );
+
+  useEffect(() => {
+    console.log(window.screen.width);
+    console.log(window.screen.width - 1024);
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [window.screen.width]);
+
+  const handleResize = () => {
+    if (window.screen.width <= 1280) {
+      const st = 2 + (window.screen.width - 1024) * (1 / 140);
+      setFloor(`${st}rem`);
+    } else {
+      setFloor('4rem');
+    }
+  };
+
   return (
     <>
-      <div className="absolute flex items-center justify-center w-full h-full border-4">
+      {/* <div className="absolute flex items-center justify-center w-full h-full border-4">
         <img
           aria-label="마카롱"
           className="absolute object-contain scale-110 mt-[21rem]"
@@ -21,7 +47,7 @@ function Main(): JSX.Element {
         />
         <img
           aria-label="정보상"
-          className="absolute z-10 object-contain w-[13rem] h-[13rem] top-36 right-[14rem]"
+          className="absolute z-10 object-contain w-[13rem] h-[13rem] top-32 right-[14rem]"
           src="/images/toys/info.png"
           alt=""
         />
@@ -33,7 +59,7 @@ function Main(): JSX.Element {
         />
         <img
           aria-label="랭킹"
-          className="absolute z-10 object-contain w-[8rem] h-[8rem] top-80 right-[15rem]"
+          className="absolute z-10 object-contain w-[9rem] h-[9rem] top-80 right-[15rem]"
           src="/images/toys/rank.png"
           alt=""
         />
@@ -51,34 +77,48 @@ function Main(): JSX.Element {
         />
         <img
           aria-label="주식거래소"
-          className="absolute z-10 object-contain w-[15rem] h-[15rem] -mb-[25rem] ml-[42rem]"
+          className="absolute z-10 object-contain w-[15rem] h-[15rem] bottom-10 right-44"
           src="/images/toys/chart.png"
           alt=""
         />
         <img
           aria-label="가이드"
-          className="absolute z-10 object-contain w-[10rem] h-[10rem] -mb-[25rem] left-0 border-4"
+          className="absolute left-0 z-10 object-contain w-[10rem] h-[10rem] bottom-0"
           src="/images/toys/guide.png"
           alt=""
         />
-      </div>
-      <div className="w-full">
-        <Canvas
-          orthographic
-          camera={{
-            left: -1,
-            right: 1,
-            top: 1,
-            bottom: -1,
-            near: 0.1,
-            far: 1000,
-            zoom: 100
-          }}>
-          {/* <OrbitControls /> */}
-          <ambientLight intensity={0.5} />
-          <pointLight distance={2000} position={10} power={8} />
-          <Bathroom len={0.0055} pos={[0, -1.3, -8]} rot={[1.75, 0, 0.2]} />
-        </Canvas>
+      </div>*/}
+
+      <div className="items-center justify-between hidden w-full h-full max-w-screen-xl lg:flex">
+        <div className="w-[28%] h-full">1</div>
+        <div className="flex items-center justify-center h-full w-[44%] max-h-[49rem]">
+          <div className="flex justify-center items-center w-full border-4 h-[60%] mt-10 relative min-w-[14.3rem] min-h-[29.4rem]">
+            <img
+              aria-label="마카롱"
+              className={`absolute object-contain scale-110 w-full`}
+              src="/images/toys/floor.png"
+              alt=""
+              style={{ bottom: `-${floor}` }}
+            />
+            <Canvas
+              style={{ width: '100%', height: '100%' }}
+              orthographic
+              camera={{
+                left: -1,
+                right: 1,
+                top: 1,
+                bottom: -1,
+                near: 0.1,
+                far: 1000,
+                zoom: 100
+              }}>
+              <ambientLight intensity={0.5} />
+              <pointLight distance={2000} position={10} power={8} />
+              <Bathroom len={0.0055} pos={[0, -1.3, -8]} rot={[1.75, 0, 0.2]} />
+            </Canvas>
+          </div>
+        </div>
+        <div className="w-[28%] h-full">3</div>
       </div>
     </>
   );
