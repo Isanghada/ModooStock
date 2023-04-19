6ce02c32909e8966fb4ee8947c4150a9bea82d3f
rlthdqls <rlthdqls@gmail.com>
2023-04-19 17:23:37 +0900

    Fix : 수정 UserAssetLocation 구조 변경

--- back-server/src/main/java/com/server/back/domain/store/entity/UserAssetLocation.java
+++ back-server/src/main/java/com/server/back/domain/store/entity/UserAssetLocation.java
@@ -1,29 +1,44 @@
 package com.server.back.domain.store.entity;
 
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name="user_asset_location")
 @Getter
-@Builder
+@SuperBuilder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class UserAssetLocation {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class UserAssetLocation extends UserAssetEntity {
 
-    @OneToOne
-    private UserAssetEntity userAsset;
-
+    @Column(nullable = false)
     private float posX;
+
+    @Column(nullable = false)
     private float posY;
+
+    @Column(nullable = false)
     private float posZ;
+
+    @Column(nullable = false)
     private float rotX;
+
+    @Column(nullable = false)
     private float rotY;
+
+    @Column(nullable = false)
     private float rotZ;
+
+    public void init(){
+        this.posX=0F;
+        this.posY=0F;
+        this.posZ=0F;
+        this.rotX=0F;
+        this.rotY=0F;
+        this.rotZ=0F;
+    }
 
 
 }
