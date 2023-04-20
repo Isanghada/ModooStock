c33b81550c95ccb7f1d7c64cb4d878948a682fd4
rlthdqls <rlthdqls@gmail.com>
2023-04-20 15:21:25 +0900

    Feat : 추가 Swagger authorization

--- back-server/src/main/java/com/server/back/config/SwaggerConfig.java
+++ back-server/src/main/java/com/server/back/config/SwaggerConfig.java
@@ -3,13 +3,20 @@ package com.server.back.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 @Configuration
@@ -25,7 +32,7 @@ public class SwaggerConfig implements WebMvcConfigurer {
 			.apis(RequestHandlerSelectors.basePackage(
 				"com.server.back")) // Controller가 들어있는 패키지. 이 경로의 하위에 있는 api만 표시됨.
 			.paths(PathSelectors.any()) // 위 패키지 안의 api 중 지정된 path만 보여줌. (any()로 설정 시 모든 api가 보여짐)
-			.build();
+			.build().securitySchemes(Arrays.asList(apiKey())).securityContexts(Arrays.asList(securityContext()));
 		//			.pathMapping("/api/v1");
 	}
 
@@ -36,6 +43,20 @@ public class SwaggerConfig implements WebMvcConfigurer {
 			.description("modoo-stock")
 			.version("1.0")
 			.build();
+	}
+	private ApiKey apiKey() {
+		return new ApiKey("JWT", "Authorization", "header");
+	}
+
+	private SecurityContext securityContext() {
+		return SecurityContext.builder().securityReferences(defaultAuth()).build();
+	}
+
+	private List<SecurityReference> defaultAuth(){
+		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+		AuthorizationScope[] auth = new AuthorizationScope[1];
+		auth[0] = authorizationScope;
+		return Arrays.asList(new SecurityReference("JWT", auth));
 	}
 
 }
