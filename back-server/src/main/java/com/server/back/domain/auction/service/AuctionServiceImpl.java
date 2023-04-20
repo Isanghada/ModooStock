090cfcd3d9d5891d07587e4cd696dd8db80ce0ce
rlthdqls <rlthdqls@gmail.com>
2023-04-20 16:01:35 +0900

    Feat : 추가 상점 Slf4j 로킹

--- back-server/src/main/java/com/server/back/domain/auction/service/AuctionServiceImpl.java
+++ back-server/src/main/java/com/server/back/domain/auction/service/AuctionServiceImpl.java
@@ -19,11 +19,13 @@ import com.server.back.domain.user.repository.UserRepository;
 import com.server.back.exception.CustomException;
 import com.server.back.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AuctionServiceImpl implements AuctionService {
