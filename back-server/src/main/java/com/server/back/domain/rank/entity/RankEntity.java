b9e554d64047b57c599f518ad1a273302c80603a
rlthdqls <rlthdqls@gmail.com>
2023-04-27 16:43:13 +0900

    Fix : 수정 table name

--- back-server/src/main/java/com/server/back/domain/rank/entity/RankEntity.java
+++ back-server/src/main/java/com/server/back/domain/rank/entity/RankEntity.java
@@ -2,19 +2,17 @@ package com.server.back.domain.rank.entity;
 
 import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
+@Table(name="rank_table")
 public class RankEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
