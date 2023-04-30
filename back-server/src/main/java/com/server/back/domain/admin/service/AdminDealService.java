3dce725d0287b37de406b815757ff9780041ed71
susu4788 <susu4788@naver.com>
2023-04-30 22:01:15 +0900

    Feat : 추가 관리자 service init

---
+++ back-server/src/main/java/com/server/back/domain/admin/service/AdminDealService.java
@@ -0,0 +1,4 @@
+package com.server.back.domain.admin.service;
+
+public interface AdminDealService {
+}
---
+++ back-server/src/main/java/com/server/back/domain/admin/service/AdminDealServiceImpl.java
@@ -0,0 +1,12 @@
+package com.server.back.domain.admin.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AdminDealServiceImpl implements AdminDealService{
+    
+}
