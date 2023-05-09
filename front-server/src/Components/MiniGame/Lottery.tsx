d9b63cd0a499cdba34e8f932c5a239c46c85af3a
sha990403 <sha990403@gmail.com>
2023-05-09 17:52:08 +0900

    Fix : 변경 임시 로또 설명 변경

--- front-server/src/Components/MiniGame/Lottery.tsx
+++ front-server/src/Components/MiniGame/Lottery.tsx
@@ -18,7 +18,7 @@ type LotteryProps = {
 const LotteryCard = ({ title, backgroundImage, color, onClick, description, descriptionColor }: LotteryProps) => {
   return (
     <div
-      className={`flex flex-col w-[25%] lg:min-w-[20%] lg:w-[23%] mx-2 text-center border-2 rounded-[2rem] border-[${color}]/60 bg-cover bg-center`}
+      className={`flex flex-col w-[25%] lg:min-w-[20%] lg:w-[23%] mx-2 text-center border-4 rounded-[2rem] border-[${color}]/60 bg-cover bg-center shadow-md shadow-gray-400`}
       style={{ backgroundImage: `url(${backgroundImage})` }}>
       <div className="py-6 md:py-8 lg:py-10">
         <span className={`font-extrabold text-[1.2rem] md:text-[1.5rem] lg:text-[2rem] text-[${color}]`}>{title}</span>
@@ -34,11 +34,11 @@ const LotteryCard = ({ title, backgroundImage, color, onClick, description, desc
       </div>
       <div className="pb-4 lg:pb-5">
         <button
-          className={`px-4 py-1 mx-auto font-extrabold text-white rounded-3xl w-[60%]  text-[0.8rem] md:text-[0.9rem] lg:text-[1.1rem] hover:scale-110 transition-all duration-300 ${
+          className={`px-4 py-1 mx-auto font-extrabold text-white rounded-3xl w-[60%]  text-[0.8rem] md:text-[0.9rem] lg:text-[1.1rem] hover:scale-110 transition-all duration-300 shadow-sm shadow-gray-400 ${
             title === '스피드 복권' ? 'bg-[#FFC24E]/80 hover:bg-[#FFC24E]' : 'bg-[#2C94EA] hover:bg-[#2C94EA]'
           }`}
           onClick={onClick}>
-          구매 하기
+          구매하기
         </button>
       </div>
     </div>
@@ -122,7 +122,7 @@ function Lottery(): JSX.Element {
           backgroundImage={process.env.REACT_APP_S3_URL + '/images/logos/IntroDarkBG.png'}
           color="#748DA6"
           onClick={() => handleLotteryClick(true)}
-          description={['모 아님 도', '백만원으로 10억원 vs 꽝']}
+          description={['모 아님 도', '백만원으로 7억원 vs 꽝']}
           descriptionColor="#ffffff"
         />
       </div>
