76cc749683cd89fdf8e7beb89a849eea73bd3bbb
한상현 <xhaktm3344@gmail.com>
2023-05-17 01:20:34 +0900

    Feat : 수정 구글플레이스토어 재업로드

--- front-server/public/firebase-messaging-sw.js
+++ front-server/public/firebase-messaging-sw.js
@@ -29,12 +29,12 @@ self.addEventListener('push', function (e) {
     tag: resultData.tag,
     ...resultData
   };
-  console.log('2 push: ', { resultData, notificationTitle, notificationOptions });
-  // 내 정보 이벤트실행시 API
-  const getUsers = () => {
-    console.log("유저정보가져오기")
-  }
-  getUsers();
+  // console.log('2 push: ', { resultData, notificationTitle, notificationOptions });
+  // // 내 정보 이벤트실행시 API
+  // const getUsers = () => {
+  //   console.log("유저정보가져오기")
+  // }
+  // getUsers();
 
 
   self.registration.showNotification(notificationTitle, notificationOptions);
